6996e102dadbc7da00ec83ef8cf68a44d92354e9
nathan <ing.nathany@gmail.com>
2019-01-30 22:10:17 -0800

    add colored attention output

---
+++ scripts/attention/attimg.py
@@ -0,0 +1,39 @@
+import numpy as np
+import sys
+import cv2
+import os
+
+import seaborn as sns
+
+def draw_attention(attimg, n_bins=50, backgroud=1):
+  palette = sns.color_palette("RdBu", n_colors=n_bins)
+  bins = np.linspace(0, 1, n_bins)
+
+  attimg = cv2.resize(attimg, fx=3., fy=3., dsize=(0,0),
+    interpolation=cv2.INTER_LINEAR)
+
+  imgout = np.dstack([np.zeros_like(attimg)]*3)
+  background_mask = attimg == 0
+
+  # now that we have the real background, 
+  # we have to level out a low baseline:
+  attimg -= attimg[1-background_mask].min()
+  attimg *= (1. / attimg.max())
+  # attimg = -np.log(1-attimg)
+
+  # and bin it
+  attimg = np.digitize(attimg, bins)
+
+  for b in range(n_bins):
+    b_mask = attimg == b
+    imgout[b_mask, ...] = palette[b]
+
+  imgout[background_mask] = backgroud
+  imgout *= 255
+  imgout = imgout.astype(np.uint8)
+
+  return imgout
+
+if __name__ == '__main__':
+  sampleimg = np.random.uniform(low=0, high=255, size=(96,96))
+  imgout = draw_attention(sampleimg / 255.)

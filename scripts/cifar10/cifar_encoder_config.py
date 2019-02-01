ff190d990cb70729486270636a3c538ae48acd43
nathan <ing.nathany@gmail.com>
2019-02-01 10:29:58 -0800

    local cifar config

---
+++ scripts/cifar10/cifar_encoder_config.py
@@ -0,0 +1,8 @@
+encoder_args = {
+    'depth_of_model': 48,
+    'growth_rate': 42,
+    'num_of_blocks': 4,
+    'output_classes': 10,
+    'num_layers_in_each_block': 12,
+    'pool_initial': False,
+}

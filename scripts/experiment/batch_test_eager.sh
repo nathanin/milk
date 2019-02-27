082f516f864382dc504f549cc28cf6e6c8c100d6
nathan <ing.nathany@gmail.com>
2019-02-27 13:35:41 -0800

    rename pretaining according to dataset

--- scripts/experiment/batch_test_eager.sh
+++ scripts/experiment/batch_test_eager.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
+# Test for some condition filtered by MIL type:
+# example:
+#   
+#   batch_test_eager.sh attention
+
 echo $1
 for pth in $( ls args/*.txt ); do
+  # extract the timestamp from path
   p=${pth##*/}
   p=${p%.*}
 
   if grep -xq mil.$1 $pth
   then
     echo testing $p
+    python test_eager.py --timestamp $p --odir result_test --mil $1
     python test_eager.py --timestamp $p --odir result_test_mcdrop --mil $1 --mcdropout
   else
     echo skipping $p
